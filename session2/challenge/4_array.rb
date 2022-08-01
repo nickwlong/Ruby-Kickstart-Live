2f2d03278c3e08af96776a8f45fa0f168ad6e263
nickwlong <nickwlong@gmail.com>
2022-08-01 15:34:54 +0100

    Challenge 2:4

--- session2/challenge/4_array.rb
+++ session2/challenge/4_array.rb
@@ -7,3 +7,16 @@
 # get_squares [25, 4, 9, 6, 50, 16, 5] # => [4, 5]
 
 # This time you will have to define the method, it's called: get_squares
+
+def get_squares(arr)
+    squares = []
+    arr.each do |e|
+        squ = e**2
+        if arr.any?(squ)
+            squares.push(e)
+        end
+    end
+    squares.sort()
+end
+
+get_squares([25,4,9,6,50,16,5])
