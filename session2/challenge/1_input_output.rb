eda2c1d1892f46ee3463f8b60489d3edf2930465
nickwlong <nickwlong@gmail.com>
2022-08-01 14:26:15 +0100

    Challenge 2:1

--- session2/challenge/1_input_output.rb
+++ session2/challenge/1_input_output.rb
@@ -8,5 +8,10 @@
 # "11\n7\n18\n" to standard output.
 
 def sum_difference_product
-  # your code goes here
+  x = gets
+  x = x.split(" ").map{|str| str.to_i}
+  sum = x.sum
+  difference = (x[0])-(x[1])
+  product = x[0] * x[1]
+  puts "#{sum}\n#{difference}\n#{product}\n"
 end
